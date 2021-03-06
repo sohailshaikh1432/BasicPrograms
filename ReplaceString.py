45f875b4b266c35da6a75584b96e46e1da6e342e
“sohailshaikh1432” <sohailshaikh1432.ss@gmail.com>
2021-03-06 12:46:25 +0530

    [Sohail] User Input and Replace String Template “Hello <<UserName>>, How are you?”

---
+++ ReplaceString.py
@@ -0,0 +1,8 @@
+def main():
+    # Replace String
+    text = "Hello UserName How are you?"
+    replace_text = text.replace("UserName", "Sohail Shaikh")
+    print(replace_text)
+
+if __name__ == "__main__":
+    main()
