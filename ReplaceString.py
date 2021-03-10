cb933e7d79d3971894079cd42b5ec2bb689bf22e
sohailshaikh1432 <sohailshaikh1432.ss@gmail.com>
2021-03-10 14:38:03 +0530

    [Sohai]Deleted : Old files deleting because it's rduntatnt

--- ReplaceString.py
+++
@@ -1,8 +0,0 @@
-def main():
-    # Replace String
-    text = "Hello UserName How are you?"
-    replace_text = text.replace("UserName", "Sohail Shaikh")
-    print(replace_text)
-
-if __name__ == "__main__":
-    main()
