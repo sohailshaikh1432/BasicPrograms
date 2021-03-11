6d861c2f9afe8f1566b58464b81059eda91b68eb
sohailshaikh1432 <sohailshaikh1432.ss@gmail.com>
2021-03-11 10:04:23 +0530

    [Sohail]Added : Reading in 2D arrays of integers, doubles, or booleans from standard input and printing them out to standard output.

---
+++ two_dim_array.py
@@ -0,0 +1,16 @@
+# Desc -> A library for reading in 2D arrays of integers, doubles, or booleans from standard input and printing them out to standard output.
+# I/P -> M rows, N Cols, and M * N inputs for 2D Array. Use Java Scanner Class
+# Logic -> create 2 dimensional array in memory to read in M rows and N cols
+# O/P -> Print function to print 2 Dimensional Array. In Java use PrintWriter with OutputStreamWriter to print the output to the screen.
+
+
+from numpy import array
+num_row = input("Enter number of rows : ")
+num_col = input("Enter number of column : ")
+
+rows = int(input("Enter rows input : "))
+colums = input("Enter number of columns : ")
+arr = []
+arr = [num_row, num_col]
+for arr in range(arr):
+    print(arr)
